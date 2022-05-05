f1d6c8454e40f91b7e38791a7c4db67c8b4c1339
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2022-05-05 18:14:50 -0400

    2nd commit

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,8 @@
 from pip._vendor import requests as r
 
-poke_list=['ditto','pikachu','eevee','snorlax','garchomp','lucario','charizard','gardevoir','piplup','charmander','mewtwo','mew','dialga','palkia','gyarados','bulbasaur','squirtle','lopunny','infernape','combee']
+poke_list = ['ditto', 'pikachu', 'eevee', 'snorlax', 'garchomp', 'lucario', 'charizard', 'gardevoir', 'piplup',
+             'charmander', 'mewtwo', 'mew', 'dialga', 'palkia', 'gyarados', 'bulbasaur', 'squirtle', 'lopunny', 'infernape', 'combee']
+
 
 class Pokemon_Info():
     def __init__(self, name):
@@ -16,21 +18,35 @@ class Pokemon_Info():
     def sort_poke(self):
         self.weight = self.request['weight']
         for i in range(len(self.request['abilities'])):
-            self.ability.append(self.request['abilities'][i]['ability']['name'])
+            self.ability.append(
+                self.request['abilities'][i]['ability']['name'].title())
         self.type = self.request['types'][0]['type']['name'].title()
         self.info = {'Name': self.name, 'Type': self.type,
                      'Abilities': self.ability, 'Wegiht': self.weight}
-    
+
     def poke_info(self):
-        card = {'Name': self.name, 'Type':self.type , 'Ability': self.ability, 'Weight': self.weight}
-        print(card)
+        self.card = {'Name': self.name.title(), 'Type': self.type,
+                     'Ability': self.ability, 'Weight': self.weight}
+        return self.card
+
+
+class Pokedex_info():
+    def __init__(self):
+        self.pokedexlib = []
+
+    def lib_creation(self):
+        for x in poke_list:
+            poke_card = Pokemon_Info(x)
+            poke_card.getandlabel()
+            poke_card.sort_poke()
+            poke_card.poke_info()
+            self.pokedexlib.append(poke_card.poke_info())
+        print(*self.pokedexlib, sep='\n')
 
 
 def run():
-    for x in poke_list:
-        poke_card = Pokemon_Info(x)
-        poke_card.getandlabel()
-        poke_card.sort_poke()
-        poke_card.poke_info()
+    pokedex = Pokedex_info()
+    pokedex.lib_creation()
+
 
 run()
