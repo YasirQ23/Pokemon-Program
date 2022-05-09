a8e95bb495abe29201577a76511a72292ee976b4
Yasir Qureshi <yasirqureshi@Yasirs-MacBook-Air.local>
2022-05-08 22:11:26 -0400

    bug fixes

--- pokemon.py
+++ pokemon.py
@@ -43,14 +43,14 @@ class Pokedex_info():
             self.pokedexlib.append(poke_card.poke_info())
 
     def ui(self):
-        print(f'\nWelcome to Yasir\'s Pokemon Adoption Center')
+        print(f'\nWelcome to Yasir\'s Pokemon Resource Center')
         while True:
             x = input(
                 "\nMain Menu:\n[1] Search Pokemon by type\n[2] Team Builder\n[3] Get Pokemon info\n[4] Quit\nChoose An Option By Simply Typing The Number Below.\n")
             if x == '1':
                 typename = ['Normal', 'Fighting', 'Fire', 'Water', 'Flying', 'Grass', 'Poison', 'Electric',
-                            'Ground', 'Psychic', 'Rock', 'Ice', 'Bug', 'Dragon', 'Ghost', 'Dark', 'Steel', 'Fairy']
-                print(f'The Pokemon types are:\n{", ".join(typename)}')
+                            '\n''Ground', 'Psychic', 'Rock', 'Ice', 'Bug', 'Dragon', 'Ghost', 'Dark', 'Steel', 'Fairy']
+                print(f'\nThe Pokemon types are:\n{", ".join(typename)}')
                 y = input(
                     '\nWhat type of Pokemons would you like to see?: \n').title()
                 if y in typename:
@@ -63,43 +63,49 @@ class Pokedex_info():
             elif x == '2':
                 while True:
                     x = input(
-                        "\nTeam Builder Menu:\n[1] Add Pokemon to team\n[2] Remove Pokemon from team\n[3] View Pokemon team\n[4] Quit\nChoose An Option By Simply Typing The Number Below.\n")
+                        "\nTeam Builder Menu:\n[1] Add Pokemon to team\n[2] Remove Pokemon from team\n[3] View Pokemon team\n[4] Return to Main Menu\nChoose An Option By Simply Typing The Number Below.\n")
                     if x == '1':
                         if len(self.currentteam) == 4:
                             print(
                                 f'Sorry you have to many Pokemon on your team.\nThe team max is 4 Pokemons, If you would like to add a new pokemon\nPlease remove one from your team first.')
                             break
                         y = input(
-                            f'Which Pokemon would you like to add to your team?:\n').title()
+                            f'\nWhich Pokemon would you like to add to your team?:\n').title()
                         namelist = [x['Name'] for x in self.pokedexlib]
                         if y in namelist:
                             self.currentteam.append(y)
-                            print(f'{y} was succesfully added to your team.')
+                            print(f'\n{y} was succesfully added to your team.')
                         else:
-                            print(f'Invalid Input. That Pokemon does not exist.')
+                            print(
+                                f'Invalid Input. That Pokemon does not exist in our database.')
                     elif x == '2':
                         if self.currentteam == []:
                             print(
                                 f'Your current team is empty.\nReturning to Team Builder Meni')
                         else:
                             print(
-                                f'This is your current team {", ".join(self.currentteam)}')
-                            y = (f'Which Pokemon would you like to remove?:\n')
+                                f'\nThe Pokemon currently in your team are:\n{", ".join(self.currentteam)}')
+                            y = input(
+                                f'Which Pokemon would you like to remove?:\n').title()
                             if y in self.currentteam:
                                 self.currentteam.remove(y)
+                                print(
+                                    f'{y} was successfully removed from team!')
                             else:
                                 print(
-                                    f'Invalid Input.That Pokemon is not in your team!')
+                                    f'Invalid Input. That Pokemon is not in your team!\n Returning to Team Builder Menu')
                     elif x == '3':
                         if self.currentteam != []:
                             print(
                                 f'This is your current team:\n{", ".join(self.currentteam)}')
+                        else:
+                            print(f'Your team is currently empty')
                     elif x == '4':
                         break
             elif x == '3':
                 namelist = [x['Name'] for x in self.pokedexlib]
                 y = input(
-                    f'\nTo view a Pokemons basic info,\nPlease enter the name below:\n').title()
+                    f'\nSimply enter the Pokemons name below to view its basic info card.\n').title()
                 if y in namelist:
                     for x in self.pokedexlib:
                         if x['Name'] == y:
@@ -108,10 +114,10 @@ class Pokedex_info():
                                 f"\n{card['Name']} | {' / '.join(card['Type'])} Type | Abilities: {', '.join(card['Ability'])} | {int(card['Weight']*.220462)} lbs")
                 else:
                     print(
-                        f'Invalid Input. We do not have that Pokemon or it does not exist.')
+                        f'\nInvalid Input. That Pokemon does not exist in our database.')
             elif x == '4':
-                print(f'Thanks for visiting have a nice day!')
-                return False
+                print(f'Thanks For Stopping By Have Nice Day!')
+                break
 
 
 def run():
